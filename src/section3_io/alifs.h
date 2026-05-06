f131a477e82335706ab6a5631040ceb4fa25cac6
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-05-06 23:21:04 +0300

    Create alifs.h

---
+++ src/section3_io/alifs.h
@@ -0,0 +1,23 @@
+#ifndef ALIFS_H
+#define ALIFS_H
+
+#include <stdint.h>
+
+#define ALIFS_START_LBA 20000  // Safe area on your disk
+#define MAX_FILES 16           // 16 files fit in one 512-byte sector
+#define FILENAME_LEN 32
+
+typedef struct {
+    char filename[FILENAME_LEN];
+    uint32_t start_lba;
+    uint32_t size;
+    uint8_t active;   // 1 if file exists, 0 if deleted
+} __attribute__((packed)) alifs_inode_t;
+
+// Filesystem Functions
+void alifs_format();
+int  alifs_create(char* name, char* data);
+char* alifs_read(char* name);
+void alifs_list();
+
+#endif
