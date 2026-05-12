7b03077ba8baebef25ccd970d87591000871cd35
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-05-12 09:48:17 +0300

    Update kernel.c, bring back lock

--- src/kernel.c
+++ src/kernel.c
@@ -217,7 +217,8 @@ void kernel_main() {
 
     while (inb(0x64) & 0x01) { inb(0x60); }
 
-
+    lock_system_hardened();
+    
     vga_write("AliOS 4 - made by a 12yo - Multi-TTY Mode\n");
     vga_write("System Ready. Use Ctrl+Alt+F1-F10 to switch.\n");
     vga_write("> ");
