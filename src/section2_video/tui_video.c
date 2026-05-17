91fc512fbaf43bab63feea4f8aa4e40ba152fda2
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-05-17 22:57:35 +0300

    Update tui_video.c

--- src/section2_video/tui_video.c
+++ src/section2_video/tui_video.c
@@ -1,47 +1,57 @@
+/* Copyright (c) 2026 Ali  
+All rights reserved.
+*/
 #include "tui_video.h"
-
-#define TEXT_MEM ((volatile uint16_t*)0xB8000)
+#include "../section1_cpu/io.h" // For cursor manipulation if needed, maybe will put soon
 
 void tui_clear(uint8_t attribute) {
-    uint16_t clear_cell = (uint16_t)' ' | ((uint16_t)attribute << 8);
-    for (int i = 0; i < COLS * ROWS; i++) {
-        TEXT_MEM[i] = clear_cell;
+    volatile uint16_t* vga = (volatile uint16_t*)VGA_MEM_ADDR;
+    uint16_t blank = (uint16_t)' ' | ((uint16_t)attribute << 8);
+    for (int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
+        vga[i] = blank;
     }
 }
 
-void tui_draw_char(int x, int y, char c, uint8_t attribute) {
-    if (x >= 0 && x < COLS && y >= 0 && y < ROWS) {
-        uintptr_t offset = ((uintptr_t)y * (uintptr_t)COLS) + (uintptr_t)x;
-        TEXT_MEM[offset] = (uint16_t)c | ((uint16_t)attribute << 8);
+void tui_print_at(int x, int y, const char* str, uint8_t attribute) {
+    if (x < 0 || x >= VGA_WIDTH || y < 0 || y >= VGA_HEIGHT) return;
+    volatile uint16_t* vga = (volatile uint16_t*)VGA_MEM_ADDR + (y * VGA_WIDTH + x);
+    
+    while (*str && x < VGA_WIDTH) {
+        *vga++ = (uint16_t)*str++ | ((uint16_t)attribute << 8);
+        x++;
     }
 }
 
-void tui_draw_string(int x, int y, const char* str, uint8_t attribute) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        tui_draw_char(x + i, y, str[i], attribute);
-    }
-}
+void tui_draw_box(int x, int y, int width, int height, const char* title, uint8_t attribute) {
+    volatile uint16_t* vga = (volatile uint16_t*)VGA_MEM_ADDR;
 
-void tui_draw_window(int start_x, int start_y, int w, int h, const char* title, uint8_t attr) {
-    for (int x = start_x; x < start_x + w; x++) {
-        tui_draw_char(x, start_y, (char)0xCD, attr);         
-        tui_draw_char(x, start_y + h - 1, (char)0xCD, attr); 
+    // Draw Corners & Horizontal lines
+    for (int i = x + 1; i < x + width - 1; i++) {
+        if (i >= 0 && i < VGA_WIDTH) {
+            if (y >= 0 && y < VGA_HEIGHT) vga[y * VGA_WIDTH + i] = (uint16_t)0xCD | (attribute << 8);         // Double line top
+            if ((y + height - 1) >= 0 && (y + height - 1) < VGA_HEIGHT) vga[(y + height - 1) * VGA_WIDTH + i] = (uint16_t)0xCD | (attribute << 8); // Double line bottom
+        }
     }
-    for (int y = start_y; y < start_y + h; y++) {
-        tui_draw_char(start_x, y, (char)0xBA, attr);         
-        tui_draw_char(start_x + w - 1, y, (char)0xBA, attr); 
-    }
-    tui_draw_char(start_x, start_y, (char)0xC9, attr);                 
-    tui_draw_char(start_x + w - 1, start_y, (char)0xBB, attr);         
-    tui_draw_char(start_x, start_y + h - 1, (char)0xC8, attr);         
-    tui_draw_char(start_x + w - 1, start_y + h - 1, (char)0xBC, attr); 
 
-    if (title != 0) {
+    // Draw Vertical lines
+    for (int j = y + 1; j < y + height - 1; j++) {
+        if (j >= 0 && j < VGA_HEIGHT) {
+            if (x >= 0 && x < VGA_WIDTH) vga[j * VGA_WIDTH + x] = (uint16_t)0xBA | (attribute << 8);         // Vertical left
+            if ((x + width - 1) >= 0 && (x + width - 1) < VGA_WIDTH) vga[j * VGA_WIDTH + (x + width - 1)] = (uint16_t)0xBA | (attribute << 8); // Vertical right
+        }
+    }
+
+    // Setup Box Corner Connectors
+    if (x >= 0 && x < VGA_WIDTH && y >= 0 && y < VGA_HEIGHT)                 vga[y * VGA_WIDTH + x] = (uint16_t)0xC9 | (attribute << 8);
+    if ((x + width - 1) >= 0 && (x + width - 1) < VGA_WIDTH && y >= 0 && y < VGA_HEIGHT) vga[y * VGA_WIDTH + (x + width - 1)] = (uint16_t)0xBB | (attribute << 8);
+    if (x >= 0 && x < VGA_WIDTH && (y + height - 1) >= 0 && (y + height - 1) < VGA_HEIGHT) vga[(y + height - 1) * VGA_WIDTH + x] = (uint16_t)0xC8 | (attribute << 8);
+    if ((x + width - 1) >= 0 && (x + width - 1) < VGA_WIDTH && (y + height - 1) >= 0 && (y + height - 1) < VGA_HEIGHT) vga[(y + height - 1) * VGA_WIDTH + (x + width - 1)] = (uint16_t)0xBC | (attribute << 8);
+
+    // Print Title if available
+    if (title) {
         int title_len = 0;
-        while (title[title_len] != '\0') title_len++;
-
-        tui_draw_char(start_x + 2, start_y, '[', attr);
-        tui_draw_string(start_x + 3, start_y, title, ATTR_APP_BOX);
-        tui_draw_char(start_x + 3 + title_len, start_y, ']', attr);
+        while (title[title_len]) title_len++;
+        int title_x = x + (width - title_len) / 2;
+        tui_print_at(title_x, y, title, attribute);
     }
 }
