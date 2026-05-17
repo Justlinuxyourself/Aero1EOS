fd09a39de667ebed8baa1fb658ed3c040164d1d9
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-05-17 22:30:16 +0300

    Update shell.c

--- src/section4_shell/shell.c
+++ src/section4_shell/shell.c
@@ -31,6 +31,7 @@ extern void sleep_ms();
 extern uint32_t ide_get_total_sectors();
 extern unsigned int get_uptime_ms();
 extern char wait_for_key();
+extern void cmd_start_gui();
 extern int strncmp(const char* s1, const char* s2, int n);
 int alifs_is_directory(char* name);
 // Simple PRNG state
@@ -1283,6 +1284,9 @@ void cmd_divbyzero(char* args) {
     
     (void)result; 
 }
+void gui() {
+  cmd_start_gui();
+}
 /* --- Shell Logic --- */
 void shell_register_command(const char* name, const char* desc, command_func func) {
     command_node_t* new_node = (command_node_t*)kmalloc(sizeof(command_node_t));
@@ -1345,6 +1349,7 @@ void shell_init() {
     shell_register_command("asma", "Random Name of Allah and its meaning", cmd_asma);
     shell_register_command("install", "Install AliOS", install_aos);
     shell_register_command("divbyzero", "DivbyZero", cmd_divbyzero);
+    shell_register_command("gui", "GUI", gui);
 }
 
 void shell_dispatch(char* buffer) {
