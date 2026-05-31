89efa9e747d4f4e1a72c821d6361724cc20738e5
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-05-31 21:10:39 +0300

    Update tss.c

--- src/section7_posix/tss.c
+++ src/section7_posix/tss.c
@@ -17,3 +17,18 @@ void setup_tss(uint64_t kernel_stack) {
     tss.rsp0 = kernel_stack;
     tss.iopb_offset = sizeof(tss_t);
 }
+
+extern uint16_t tss_base_low;
+extern uint8_t  tss_base_mid;
+extern uint8_t  tss_base_high;
+extern uint32_t tss_base_upper;
+
+void patch_gdt_tss() {
+    uint64_t base = (uint64_t)&tss;
+
+    tss_base_low   = (uint16_t)(base & 0xFFFF);
+    tss_base_mid   = (uint8_t)((base >> 16) & 0xFF);
+    tss_base_high  = (uint8_t)((base >> 24) & 0xFF);
+    tss_base_upper = (uint32_t)(base >> 32);
+}
+
