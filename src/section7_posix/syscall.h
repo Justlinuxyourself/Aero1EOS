6e0318d05fe64757740bed68661d9fbc69e1b492
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-06-01 17:06:29 +0300

    Update syscall.h

--- src/section7_posix/syscall.h
+++ src/section7_posix/syscall.h
@@ -9,10 +9,13 @@
 #define SYS_EXIT  4
 
 typedef struct {
-    uint64_t r15, r14, r13, r12, r11, r10, r9, r8;
-    uint64_t rbp, rdi, rsi, rdx, rcx, rbx, rax;
+    // 1. Pushed by isr_syscall:
+    uint64_t rax, rbx, rcx, rdx, rsi, rdi, r8, r9, r10, r11, r12, r13, r14, r15, rbp;
+    
+    // 2. Pushed by the CPU hardware on 'int 0x80':
     uint64_t rip, cs, rflags, rsp, ss;
-} registers_t;
+} __attribute__((packed)) registers_t;
+
 
 void init_syscalls();
 void syscall_handler(registers_t *regs);
