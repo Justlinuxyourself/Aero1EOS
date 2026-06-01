3b1d59f91d539f7e1a50541e88b51f6f70e20e31
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-06-01 17:07:49 +0300

    Update syscall_stub.asm

--- src/section7_posix/syscall_stub.asm
+++ src/section7_posix/syscall_stub.asm
@@ -2,14 +2,15 @@ extern syscall_handler
 
 global isr_syscall
 isr_syscall:
+    ; CPU already pushed: SS, RSP, RFLAGS, CS, RIP
     push rbp
     push r15; push r14; push r13; push r12; push r11; push r10; push r9; push r8
     push rdi; push rsi; push rdx; push rcx; push rbx; push rax
     
-    mov rdi, rsp
+    mov rdi, rsp           ; rdi points to the registers_t struct
     call syscall_handler
     
     pop rax; pop rbx; pop rcx; pop rdx; pop rsi; pop rdi
     pop r8; pop r9; pop r10; pop r11; pop r12; pop r13; pop r14; pop r15
     pop rbp
-    sysretq
+    iretq                  ; Return using the hardware-pushed frame
