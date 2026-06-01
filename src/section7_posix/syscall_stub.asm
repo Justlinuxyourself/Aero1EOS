135736b3acd3236f510b6cf3f3c7f7f3f76acc9c
Justlinuxyourself <alithelinuxuser@gmail.com>
2026-06-01 04:00:21 +0300

    Update syscall_stub.asm

--- src/section7_posix/syscall_stub.asm
+++ src/section7_posix/syscall_stub.asm
@@ -12,4 +12,4 @@ isr_syscall:
     pop rax; pop rbx; pop rcx; pop rdx; pop rsi; pop rdi
     pop r8; pop r9; pop r10; pop r11; pop r12; pop r13; pop r14; pop r15
     pop rbp
-    iretq
+    sysretq
